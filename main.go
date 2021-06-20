f070399a8893296e594f0ef2e31dcbc44ca7c705
Frank Beutelschiess <frank@thesym.site>
2021-06-20 10:05:50 +0200

    restructure repository

--- main.go
+++ main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-	"thesym.site/k8s/structural/ingress/nginx"
+	"thesym.site/k8s/definition/structural/ingress/nginx"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
+
+		////////////////////////
+		// STRUCTURAL
+		//
 		err := nginx.CreateNginxIngressController(ctx)
 		return err
 
+
+		////////////////////////
+		// ALT
+		//
+		// return nil
 	})
 }
