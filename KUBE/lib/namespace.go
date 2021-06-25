2af50dfec78f51c1c2c5079e90b047bc8f8b2bc3
Frank Beutelschiess <frank@thesym.site>
2021-06-25 17:32:16 +0200

    lib: add additional labels to namespaces

--- KUBE/lib/namespace.go
+++ KUBE/lib/namespace.go
@@ -4,7 +4,7 @@ package lib
 import (
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/meta/v1"
-	p "github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 // NamespaceTier is used to group namespaces respectively apps belonging to different namespaces
@@ -28,35 +28,48 @@ const (
 	NamespaceTierTesting                                // testing
 )
 
+type NamespaceLabel struct {
+	Name  string
+	Value string
+}
+
 // Namespace are used to configurate the pulumiNamespaces.
 // Mind that bools default to false if not set otherwise at instantiation
 type Namespace struct {
-	Name          string
-	Tier          NamespaceTier
-	GlooDiscovery bool
+	Name             string
+	Tier             NamespaceTier
+	GlooDiscovery    bool
+	AdditionalLabels []NamespaceLabel
 }
 
 // CreateNamespaces takes one or more namespaces
-func CreateNamespaces(ctx *p.Context, nameSpaces ...*Namespace) error {
+func CreateNamespaces(ctx *pulumi.Context, nameSpaces ...*Namespace) error {
+
 	for _, n := range nameSpaces {
 
-		labels := p.StringMap{
-			"name": p.String(n.Name),
-			"tier": p.String(n.Tier.String()),
+		labels := pulumi.StringMap{
+			"name": pulumi.String(n.Name),
+			"tier": pulumi.String(n.Tier.String()),
 		}
 
 		if n.GlooDiscovery {
-			labels["discovery.solo.io/function_discovery"] = p.String("enabled")
+			labels["discovery.solo.io/function_discovery"] = pulumi.String("enabled")
+		}
+
+		if len(n.AdditionalLabels) > 0 {
+			for _, label := range n.AdditionalLabels {
+				labels[label.Name] = pulumi.String(label.Value)
+			}
 		}
 
 		_, err := corev1.NewNamespace(
 			ctx,
 			n.Name,
 			&corev1.NamespaceArgs{
-				ApiVersion: p.String("v1"),
-				Kind:       p.String("Namespace"),
+				ApiVersion: pulumi.String("v1"),
+				Kind:       pulumi.String("Namespace"),
 				Metadata: &metav1.ObjectMetaArgs{
-					Name:   p.String(n.Name),
+					Name:   pulumi.String(n.Name),
 					Labels: labels,
 				},
 				Spec: nil,
