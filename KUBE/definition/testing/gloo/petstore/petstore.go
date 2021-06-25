f5822c9b9f18f4ed2b0b037c0adb285ec380849c
Frank Beutelschiess <frank@thesym.site>
2021-06-25 17:34:07 +0200

    gloopetstore: add ingress

--- KUBE/definition/testing/gloo/petstore/petstore.go
+++ KUBE/definition/testing/gloo/petstore/petstore.go
@@ -5,56 +5,128 @@ import (
 	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/apps/v1"
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/meta/v1"
-	p "github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	networkingv1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/networking/v1"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"thesym.site/k8s/lib"
 )
 
-func CreateGlooPetstore(ctx *p.Context) error {
-
-	namespacePetstore := &lib.Namespace{
-		Name:          "testing-petstore",
+var (
+	namespacePetstore = &lib.Namespace{
+		Name:          "petstore",
 		Tier:          lib.NamespaceTierTesting,
 		GlooDiscovery: true,
 	}
+)
+
+func CreateGlooPetstore(ctx *pulumi.Context) error {
 
 	err := lib.CreateNamespaces(ctx, namespacePetstore)
+	if err != nil {
+		return err
+	}
+
+	err = addIngress(ctx)
 
 	if err != nil {
 		return err
 	}
 
-	_, err = appsv1.NewDeployment(ctx, "defaultPetstoreDeployment", &appsv1.DeploymentArgs{
-		ApiVersion: p.String("apps/v1"),
-		Kind:       p.String("Deployment"),
+	err = execGeneratedCode(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// TODO: extract
+func addIngress(ctx *pulumi.Context) error {
+
+	_, err := networkingv1.NewIngress(ctx, namespacePetstore.Name, &networkingv1.IngressArgs{
+		ApiVersion: pulumi.String("networking.k8s.io/v1"),
+		Kind:       pulumi.String("Ingress"),
 		Metadata: &metav1.ObjectMetaArgs{
-			Labels: p.StringMap{
-				"app": p.String("petstore"),
+			Labels:    pulumi.StringMap{"app": pulumi.String("petstore")},
+			Name:      pulumi.String("petstore"),
+			Namespace: pulumi.String(namespacePetstore.Name),
+			Annotations: pulumi.StringMap{
+				// "kubernetes.io/ingress.class": pulumi.String("nginx"),
+				// "nginx.ingress.kubernetes.io/force-ssl-redirect": pulumi.String("true"),
+				// "nginx.ingress.kubernetes.io/ssl-redirect": pulumi.String("true"),
+				// "nginx.ingress.kubernetes.io/ssl-redirect": pulumi.String("false"),
+				// "nginx.ingress.kubernetes.io/configuration-snippet": pulumi.String(
+				// 	`if ($http_x_forwarded_proto = 'http') {
+				//            return 301 https://$host$request_uri;
+				//          }`),
 			},
-			Name:      p.String("petstore"),
-			Namespace: p.String(namespacePetstore.Name),
+		},
+		Spec: &networkingv1.IngressSpecArgs{
+			Rules: networkingv1.IngressRuleArray{
+				&networkingv1.IngressRuleArgs{
+					Host: pulumi.String("pet.dev.thesym.site"),
+					Http: &networkingv1.HTTPIngressRuleValueArgs{
+						Paths: networkingv1.HTTPIngressPathArray{
+							&networkingv1.HTTPIngressPathArgs{
+								PathType: pulumi.String("Prefix"),
+								Path:     pulumi.String("/"),
+								Backend: &networkingv1.IngressBackendArgs{
+									Service: &networkingv1.IngressServiceBackendArgs{
+										Name: pulumi.String("petstore"),
+										Port: &networkingv1.ServiceBackendPortArgs{
+											Number: pulumi.Int(8080),
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		// Tls:              nil,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func execGeneratedCode(ctx *pulumi.Context) error {
+	// CHANGES: namespaceAdded
+
+	_, err := appsv1.NewDeployment(ctx, "defaultPetstoreDeployment", &appsv1.DeploymentArgs{
+		ApiVersion: pulumi.String("apps/v1"),
+		Kind:       pulumi.String("Deployment"),
+		Metadata: &metav1.ObjectMetaArgs{
+			Labels: pulumi.StringMap{
+				"app": pulumi.String("petstore"),
+			},
+			Name:      pulumi.String("petstore"),
+			Namespace: pulumi.String(namespacePetstore.Name),
 		},
 		Spec: &appsv1.DeploymentSpecArgs{
-			Replicas: p.Int(1),
+			Replicas: pulumi.Int(1),
 			Selector: &metav1.LabelSelectorArgs{
-				MatchLabels: p.StringMap{
-					"app": p.String("petstore"),
+				MatchLabels: pulumi.StringMap{
+					"app": pulumi.String("petstore"),
 				},
 			},
 			Template: &corev1.PodTemplateSpecArgs{
 				Metadata: &metav1.ObjectMetaArgs{
-					Labels: p.StringMap{
-						"app": p.String("petstore"),
+					Labels: pulumi.StringMap{
+						"app": pulumi.String("petstore"),
 					},
 				},
 				Spec: &corev1.PodSpecArgs{
 					Containers: corev1.ContainerArray{
 						&corev1.ContainerArgs{
-							Image: p.String("soloio/petstore-example:latest"),
-							Name:  p.String("petstore"),
+							Image: pulumi.String("soloio/petstore-example:latest"),
+							Name:  pulumi.String("petstore"),
 							Ports: corev1.ContainerPortArray{
 								&corev1.ContainerPortArgs{
-									ContainerPort: p.Int(8080),
-									Name:          p.String("http"),
+									ContainerPort: pulumi.Int(8080),
+									Name:          pulumi.String("http"),
 								},
 							},
 						},
@@ -69,24 +141,24 @@ func CreateGlooPetstore(ctx *p.Context) error {
 	}
 
 	_, err = corev1.NewService(ctx, "defaultPetstoreService", &corev1.ServiceArgs{
-		ApiVersion: p.String("v1"),
-		Kind:       p.String("Service"),
+		ApiVersion: pulumi.String("v1"),
+		Kind:       pulumi.String("Service"),
 		Metadata: &metav1.ObjectMetaArgs{
-			Labels: p.StringMap{
-				"service": p.String("petstore"),
+			Labels: pulumi.StringMap{
+				"service": pulumi.String("petstore"),
 			},
-			Name:      p.String("petstore"),
-			Namespace: p.String(namespacePetstore.Name),
+			Name:      pulumi.String("petstore"),
+			Namespace: pulumi.String(namespacePetstore.Name),
 		},
 		Spec: &corev1.ServiceSpecArgs{
 			Ports: corev1.ServicePortArray{
 				&corev1.ServicePortArgs{
-					Port:     p.Int(8080),
-					Protocol: p.String("TCP"),
+					Port:     pulumi.Int(8080),
+					Protocol: pulumi.String("TCP"),
 				},
 			},
-			Selector: p.StringMap{
-				"app": p.String("petstore"),
+			Selector: pulumi.StringMap{
+				"app": pulumi.String("petstore"),
 			},
 		},
 	})
