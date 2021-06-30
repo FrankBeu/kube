3ab7e640e272f1ba9fc8bf0bd84e11ed9f302999
Frank Beutelschiess <frank@thesym.site>
2021-06-30 19:53:09 +0200

    add emptyCluster-config

---
+++ KUBE/env/other/emptyCluster/emptyCluster.go
@@ -0,0 +1,9 @@
+// Package emptycluster is used to remove all resources from an cluster
+package emptycluster
+
+import (
+	"thesym.site/kube/lib"
+)
+
+// Kube is the configuration for an empty cluster
+var Kube = lib.KubeConfig{}
